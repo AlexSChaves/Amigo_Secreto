9606b0bfbf8649cea7a365bf0bbe83aebf03219d
AlexSChaves <alexsandro091092@gmail.com>
2024-02-21 16:49:06 -0300

    Configurando Ping e Interceptador

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import cors from 'cors';
 import https from 'https';
 import http from 'http';
+import siteRoutes from './routes/site';
+import { requestIntercepter } from './utils/requestIntercepter';
 
 const app = express();
 
@@ -10,8 +12,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.all('*', requestIntercepter);
+
+app.use('/', siteRoutes);
+
 const runServer = (port: number, server: http.Server) => {
-  server.listen(port, () => console.log(`🚀Running at PORT ${port}`));
+  server.listen(port, () => console.log(`🚀 Running at PORT ${port}`));
 };
 
 const regularServer = http.createServer(app);
